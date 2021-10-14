f353a4892e2b99b790068865ceaec79fb85a0a37
Socks <patricksutcliffe123@gmail.com>
2021-10-14 17:39:50 -0400

    got it to read and write posts

--- routes/notes.js
+++ routes/notes.js
@@ -7,8 +7,22 @@ const {
   } = require('../helpers/fsUtils');
 
 notes.get('/', (req,res) => {
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
 
+notes.post('/', (req,res) => {
+  console.log(req.body)
+  const { title , text } = req.body
+
+  if(req.body) {
+    const newNote = {
+      title,
+      text
+    }
+
+    readAndAppend(newNote, './db/db.json');
+    res.json("new note made!")
+  }
+})
 
 module.exports = notes
